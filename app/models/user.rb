a8bbcd1465a552ee018145ad3544fe15d0e25a0c
James Matsuba <james.matsuba@gmail.com>
2019-07-14 15:15:50 -0700

    swapped minitest for rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
 
   ["file1", "file2", 'file3'].each do |file|
     define_method "#{file}_secure_url" do
-      aws_key = read_attribute("#{file}_key")
-      aws_key.present? ? S3DownloadPresignedUrlCreator.call(aws_key) : nil
+      s3_key = read_attribute("#{file}_key")
+      s3_key.present? ? S3DownloadPresignedUrlCreator.call(s3_key) : nil
     end
 
     define_method "#{file}_filename" do
-      aws_key = read_attribute("#{file}_key")
-      aws_key.present? ? S3FilenameParser.call(aws_key) : nil
+      s3_key = read_attribute("#{file}_key")
+      s3_key.present? ? S3FilenameParser.call(s3_key) : nil
     end
   end
 
